ac93f7373dde7f6006c1cd793b5224f2c39f41f1
zihang <zzh20081022@gmail.com>
2025-06-08 10:11:45 +0800

    fix(api): 在CORS头中添加Authorization支持

--- api/sync/notes.js
+++ api/sync/notes.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   // 配置 CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Requested-With, Authorization');
   res.setHeader('Access-Control-Max-Age', '86400');
 
   if (req.method === 'OPTIONS') {
